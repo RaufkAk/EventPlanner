8c39ae5e339ff7b104898d3e4f5033d2b88c0346
Rauf Kutay Akyıldız <122848866+RaufkAk@users.noreply.github.com>
2025-12-07 10:57:39 +0300

    Pdfs+Infrastructure of Project

---
+++ config-server/src/test/java/com/yeditepe/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeditepe.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
